830e434292116c1a148c8edd8d70df568f4bc208
mdalrasel <mdalrasel.dev@gmail.com>
2025-07-02 14:19:56 +0600

    footer,about,contact section added

--- helpify-client/src/components/Footer.jsx
+++ helpify-client/src/components/Footer.jsx
@@ -1,6 +1,5 @@
 
 import { Link } from 'react-router';
-import logo from '../assets/Footer.png'
 const Footer = () => {
     return (
         <footer className="bg-gray-900 text-gray-300 py-8 mt-16">
